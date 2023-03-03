f44896e6939325cfc876c1edb61817e3de7ed9f2
maari95 <maarinagomes1@gmail.com>
2023-03-03 20:42:12 -0300

    Create seguranca.php

---
+++ SistemaPHP/seguranca.php
@@ -0,0 +1,18 @@
+<?php
+
+	function seguranca_adm(){
+
+		if((empty($_SESSION['usuarioId'])) || (empty($_SESSION['usuarioEmail'])) || (empty($_SESSION['usuarioNiveisAcessoId']))){		
+			$_SESSION['loginErro'] = "Área restrita";
+			header("Location: index.php");
+
+		}else{
+
+			if($_SESSION['usuarioNiveisAcessoId'] != "1"){
+				$_SESSION['loginErro'] = "Área restrita";
+				header("Location: index.php");
+                
+			}
+		}
+	}
+?>
