cb2e2cfef373dab6c690ebcb2c44667404ed1c45
maari95 <maarinagomes1@gmail.com>
2023-03-03 20:38:20 -0300

    Create editT.php

---
+++ SistemaPHP/editT.php
@@ -0,0 +1,25 @@
+<?php
+
+// Connect to database
+$db = mysqli_connect("localhost", "root", "", "sistema");
+
+// Check connection
+if (mysqli_connect_errno()) {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  exit();
+}
+
+$id = $_POST['id'];
+$result = mysqli_query($db, "SELECT * FROM turma WHERE idTurma = $id"); 
+$data = mysqli_fetch_assoc($result);
+
+$turma = $_POST['turma'];
+
+
+  mysqli_query ($db,"UPDATE professor SET turma = '$turma' WHERE idTurma = $id");
+
+  // Redirect to homepage
+  header("Location: listagemAluno.php");
+  exit();
+
+?>
