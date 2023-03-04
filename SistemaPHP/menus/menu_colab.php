7f78816541ee68221dbe014f54b943598af432d7
maari95 <maarinagomes1@gmail.com>
2023-03-03 23:00:46 -0300

    Create menu_colab.php

---
+++ SistemaPHP/menus/menu_colab.php
@@ -0,0 +1,36 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#"><img src="https://cdn-icons-png.flaticon.com/512/1384/1384778.png" width="30px" height="30px"></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse fixed" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="colaborador.php">Home</a>
+        </li>
+        <li class="nav-item">
+        <a class="nav-link" href="listagemAluno.php">Listagem</a>
+      </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link disabled" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aaria-disabled="true">
+            Cadastro 
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="form.php">Alunos</a></li>
+            <li><a class="dropdown-item" href="formProf.php">Professores</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="formt.php">Turmas</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+        </li>
+      </ul>
+    <?php 
+     echo ' ' .$_SESSION['usuarioNome'].  " <a href='sair.php'><button type='button' class='btn btn-dark'> Sair  </button></a>";
+    ?>
+    <!-- </form> -->
+    </div>
+  </div>
+</nav>
