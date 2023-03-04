0f317697e491b5f576fbf6c61554f69bb3d2f837
maari95 <maarinagomes1@gmail.com>
2023-03-03 21:49:20 -0300

    Update menu_admin.php

--- SistemaPHP/menus/menu_admin.php
+++ SistemaPHP/menus/menu_admin.php
@@ -4,7 +4,7 @@
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse fixed" id="navbarSupportedContent">
+    <div class="collapse navbar-collapse fixed" id="navbar">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="administrativo.php">Home</a>
